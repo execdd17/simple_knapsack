e31fdb3f7c9752901de32634d081a3f95b6ff37a
Alexander Vanadio <avanadio@gmail.com>
2012-11-12 21:43:28 -0500

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source :rubygems
+
+gem "genetic_algorithms"
---
+++ knapsack.rb
@@ -0,0 +1,53 @@
+require 'genetic_algorithms'
+
+class Knapsack
+  SIZE = 24
+  BEST_SCORE = 1000
+
+  def initialize chromosome
+    @contents = decode chromosome
+  end
+
+  def utilization
+    space_used = @contents.inject(0) do |accum, item|
+      accum += item.space_needed
+    end
+    
+    (space_used.to_f / SIZE * 1000).round
+  end
+
+  private
+
+  def decode chromosome
+    all_items = Item.create_items({ hammer: 17, coin: 4, pen: 9, wallet: 11 })
+    index = -1
+
+    filtered_items = all_items.select do |item|
+      index += 1
+      chromosome[index] == "1"
+    end
+  end
+end
+
+class Item
+  def self.create_items hash
+    hash.inject(Array.new) do |items, (name, space_needed)|
+      items << Item.new(name, space_needed)
+    end
+  end
+
+  def initialize name, space_needed
+    @name = name
+    @space_needed = space_needed
+  end
+
+  attr_reader :name, :space_needed
+end
+
+include GeneticAlgorithms
+
+Engine.new(10,4).start(Knapsack::BEST_SCORE) do |chromosome|
+  score = Knapsack.new(chromosome).utilization
+  score = Knapsack::BEST_SCORE - score if score > Knapsack::BEST_SCORE
+  score
+end
